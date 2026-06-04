dbeb1d540816940b47e78698f00133b0435947e0
Herbert0401 <c.zhang47@lancaster.ac.uk>
2026-06-04 22:56:31 +0800

    Add site thumbnail image

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ const CONTENT_TYPES = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".json": "application/json; charset=utf-8",
   ".svg": "image/svg+xml; charset=utf-8"
 };
